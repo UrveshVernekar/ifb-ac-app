6628ef310fba92999c02d36a4b7c22a4676227e3
UrveshVernekar <urveshvernekar8@gmail.com>
2026-05-28 17:19:14 +0530

    Build fix

--- types/altcha.d.ts
+++ types/altcha.d.ts
@@ -1,10 +1,14 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    "altcha-widget": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
-      challengeurl?: string;
-      auto?: string;
-      overlay?: boolean;
-      name?: string;
-    };
+import * as React from "react";
+
+declare module "react" {
+  namespace JSX {
+    interface IntrinsicElements {
+      "altcha-widget": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+        challengeurl?: string;
+        auto?: string;
+        overlay?: boolean;
+        name?: string;
+      };
+    }
   }
 }
